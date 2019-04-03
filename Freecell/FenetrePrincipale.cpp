bab0a2444355b3900aafd93312bcb71557099b5c
jean baptiste jacquet <jbapt01@live.fr>
2019-04-03 11:11:09 +0200

    Ajouts des cartes

--- Freecell/FenetrePrincipale.cpp
+++ Freecell/FenetrePrincipale.cpp
@@ -1,7 +1,7 @@
 //
 // Created by kamilcaglar on 02/04/19.
 //
-
+#include"pch.h"
 #include "FenetrePrincipale.h"
 
 using namespace std;
@@ -11,26 +11,28 @@ using namespace cimg_library;
 FenetrePrincipale::FenetrePrincipale() {
 
 // Declare an image to draw the grid
-    CImg<unsigned char> grid(300,300,1,3,255);
+    //CImg<unsigned char> grid(300,300,1,3,255);
+	CImg<unsigned char> image("imageCarte/blank.ppm"), visu(500, 400, 1, 3, 0);
 // Declare a display to draw the scene
-    CImgDisplay disp(grid,"FreeCell",0,false,false);
+    //CImgDisplay disp(grid,"FreeCell",0,false,false);
+	CImgDisplay carte(image, "Carte", 0, false, false);
     // Center the window on the screen
-    disp.move((CImgDisplay::screen_width() - disp.width())/2,
-              (CImgDisplay::screen_height() - disp.height())/2);
+    carte.move((CImgDisplay::screen_width() - carte.width())/2,
+              (CImgDisplay::screen_height() - carte.height())/2);
 
 // Declare an image to display the scene
-    CImg<unsigned char> scene=grid;
-
+    //CImg<unsigned char> scene=grid;
+	CImg<unsigned char> scene = image;
 
 // Main loop, exit if the display window is closed or if ESC or Q key is hit
-    while (!disp.is_closed() && !disp.is_keyESC() && !disp.is_keyQ()) {
+    while (!carte.is_closed() && !carte.is_keyESC() && !carte.is_keyQ()) {
 
         // Display the scene
-        scene.display(disp);
-
-        disp.wait();
+        //scene.display(disp);
+		scene.display(carte);
+        carte.wait();
 
         // Handle window resize
-        if (disp.is_resized()) disp.resize();
+        if (carte.is_resized()) carte.resize();
     }
 }
--- Freecell/Freecell.cpp
+++ Freecell/Freecell.cpp
@@ -1,6 +1,6 @@
 // Freecell.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
-
+#include "pch.h"
 #include <iostream>
 #include "Fonctions.h"
 #include "Carte.h"
